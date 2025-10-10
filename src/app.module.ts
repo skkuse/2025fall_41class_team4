d083ab82763bcc7949bf3c9f737bc5a974aff5ec
Sungmin <kangsm0903@naver.com>
2025-10-10 15:34:14 +0900

    [FEAT] Movie Part 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MoviesModule } from './movies/movies.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { DataSyncModule } from './data-sync/data-sync.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true, // 전역 설정
+  }), 
+  ScheduleModule.forRoot(),
+  MoviesModule,
+  DataSyncModule],
   controllers: [AppController],
   providers: [AppService],
 })
